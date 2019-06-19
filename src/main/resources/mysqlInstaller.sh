c7c95103bcf9c089448f42f65dc8e3915e777c8b
GuoJiafeng <18611781163@163.com>
2019-06-19 19:54:27 +0800

    mysql

--- src/main/resources/mysqlInstaller.sh
+++ src/main/resources/mysqlInstaller.sh
@@ -1,27 +1,3 @@
-#!/bin/bash
-
-echo "Java环境一键安装脚本"
-
-
-
-echo "安装Java环境（JDK&Tomcat）"
-# wget -c http://mirrors.linuxeye.com/oneinstack-full.tar.gz && tar xzf oneinstack-full.tar.gz && ./oneinstack/install.sh --tomcat_option 2 --jdk_option 2 --reboot
-
-
-
-
-
-echo "安装Maven"
-echo " --------------------"
-echo "| author：GuoJiafeng |"
-echo "| company：Baizhi    |"
-echo " --------------------"
-
-
-
-
-
-------------------------------------------------------
 echo "安装Mysql"
 echo " --------------------"
 echo "| author：GuoJiafeng |"
@@ -72,29 +48,3 @@ mysql  -e "flush privileges;"
 echo "重启Mysql"
 
 service mysql restart
-
-------------------------------------------------------
-
-
-echo "安装Git"
-
-yum remove git*
-
-wget -c "http://files.git.oschina.net/group1/M00/07/63/PaAvDFy5cemAdOGyAH6LPKZEAXw4781.gz?token=9d65cf0f4febc5979b3603b09c3052d6&ts=1555657269&attname=git-2.21.0.tar.gz&disposition=attachment" -O git-2.21.0.tar.gz
-
-
-tar -zxvf git-2.21.0.tar.gz
-
-cd git-2.21.0
-
-yum install curl-devel expat-devel gettext-devel openssl-devel zlib-devel gcc perl-ExtUtils-Embed
-
-make prefix=/usr/local/git install
-
-/usr/local/git/bin/git --version
-
-
-
-
-echo "安装Nginx"
-wget -c http://mirrors.linuxeye.com/oneinstack-full.tar.gz && tar xzf oneinstack-full.tar.gz && ./oneinstack/install.sh --nginx_option 1 --reboot
