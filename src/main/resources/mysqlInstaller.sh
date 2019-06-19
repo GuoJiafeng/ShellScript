95ce5c5fd596cf45e414570d1518276dc5851c53
GuoJiafeng <18611781163@163.com>
2019-06-19 20:21:20 +0800

    mysql

--- src/main/resources/mysqlInstaller.sh
+++ src/main/resources/mysqlInstaller.sh
@@ -17,8 +17,12 @@ rpm -e --nodeps MySQL-client-5.5.62-1.el6.x86_64
 
 rpm -e --nodeps MySQL-server-5.5.62-1.el6.x86_64
 
+echo "开始安装Mysql 客户端"
 
 rpm -ivh /root/app/MySQL-client-5.5.62-1.el6.x86_64.rpm
+
+echo "开始安装Mysql 服务端"
+
 rpm -ivh /root/app/MySQL-server-5.5.62-1.el6.x86_64.rpm
 
 
@@ -33,16 +37,14 @@ read -p "请输入您想要修改的密码："  mysqlpass01
 
 read -p "请输入ENTER继续"
 
-#mysql -uroot -e "use mysql;"
+mysql -uroot -e "use mysql;"
 
-mysql -f mysql -e "UPDATE user SET Password=PASSWORD('${mysqlpass01}') where USER='root'"
+echo "默认开启远程连接权限"
 
-mysql  -e "flush privileges;"
-
-
-echo "开启远程连接权限"
 mysql -f mysql -e "delete from user where password='';"
+
 mysql -f mysql -e "grant all privileges on *.* to 'root'@'%' identified by '${mysqlpass01}' with grant option;"
+
 mysql  -e "flush privileges;"
 
 
