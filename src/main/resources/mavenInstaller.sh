7ef925a700464ff71892b01e0d929cd36df226cc
GuoJiafeng <18611781163@163.com>
2019-06-26 11:04:13 +0800

    maven

--- src/main/resources/mavenInstaller.sh
+++ src/main/resources/mavenInstaller.sh
@@ -14,4 +14,36 @@ mkdir /home/maven
 tar -zxvf /root/app/apache-maven-3.3.9-bin.tar.gz -C /home/maven
 
 
+
 echo "开始配置环境变量"
+
+
+echo "export MAVEN_HOME=/home/maven/apache-maven-3.3.9/" >> /root/.bashrc
+
+source /root/.bashrc
+
+echo "export PATH=$PATH:$MAVEN_HOME/bin" >> /root/.bashrc
+
+source /root/.bashrc
+
+sleep 3
+
+echo "配置完成"
+
+sleep 3
+
+echo "开始配置Maven"
+
+echo "创建本地仓库 /home/maven/local"
+
+mkdir /home/maven/local
+
+echo "配置阿里云镜像源"
+
+rm -rf $MAVEN_HOME/conf/settings.xml
+
+
+cp settings.xml   $MAVEN_HOME/conf/
+
+
+echo "Maven相关配置完成"
